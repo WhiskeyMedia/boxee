3e6743bd8c378a1bc51848398db8e0306f982780
sdornan <sam@whiskeymedia.com>
2011-07-28 17:37:50 -0700

    All pretty

--- giantbomb/start.py
+++ giantbomb/start.py
@@ -1,18 +1,8 @@
-# import mc
-# import menu
+import mc
+import menu
 
-# '''
-# here we can do some pre launch processing if we wish.
-# checking authentication or pre-loading content, anything we need.
-# '''
+mc.ActivateWindow(14000)
 
-# mc.ShowDialogOk('Boxee Basic Python App', 'Hello! Welcome to this basic python app demo!')
-
-# if mc.ShowDialogConfirm('Boxee Basic Python App', 'Would you like to launch the application?', 'No', 'Yes!'):
-# 	mc.ActivateWindow(14000)
-
-# categories = menu.get_categories()
-# if categories:
-#     mc.GetWindow(14000).GetList(100).SetItems(categories)
-
-# mc.ActivateWindow(14000)
+categories = menu.get_categories()
+if categories:
+    mc.GetWindow(14000).GetList(100).SetItems(categories)
