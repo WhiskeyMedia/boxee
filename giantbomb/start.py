c5b5bbe9f19ade7340d3a663bb7d15556646393b
sdornan <sam@whiskeymedia.com>
2011-08-22 14:39:38 -0700

    Set latest videos to display at start

--- giantbomb/start.py
+++ giantbomb/start.py
@@ -9,3 +9,17 @@ mc.ActivateWindow(14000)
 categories = menu.get_categories()
 if categories:
     mc.GetWindow(14000).GetList(100).SetItems(categories)
+
+mc.GetActiveWindow().PushState()
+mc.GetActiveWindow().GetList(100).SetFocusedItem(0)
+
+cat_id = mc.GetFocusedItem(14000, 100).GetProperty('id')
+
+if cat_id == 'live':
+    pass
+else:
+    mc.ShowDialogWait()
+    mc.GetActiveWindow().GetList(120).SetItems(items)
+    mc.GetActiveWindow().GetControl(120).SetVisible(True)
+    mc.HideDialogWait()
+    mc.GetActiveWindow().GetControl(120).SetFocus()
