6c92535d1376f41de778c5f23231fe073237c512
sdornan <sam@whiskeymedia.com>
2011-08-22 15:16:14 -0700

    Use the_real_boxee's api key

--- giantbomb/menu.py
+++ giantbomb/menu.py
@@ -3,11 +3,9 @@ import simplejson
 import urllib
 
 API_PATH = 'http://localhost:8000'
-#API_KEY = 'e5529a761ee3394ffbd237269966e9f53a4c7bf3' # Default API key
-API_KEY = '3353202b4c9af87523cab1184e471fd1c5be4def'
+API_KEY = '57659bff0faacdf8ab9b2500b552e7fd37b4b677' # Default API key
 
 lf_category = 0
-lf_erun = 0
 lf_video = 0
 
 def get_categories():
