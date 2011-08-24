97a89ac55ce66a3bcd4dae14934bffdf06ab5cfb
sdornan <sam@whiskeymedia.com>
2011-08-24 10:08:19 -0700

    Adding comicvine folder

---
+++ comicvine/menu.py
@@ -0,0 +1,143 @@
+import mc
+import simplejson
+import urllib
+
+API_PATH = 'http://api.comicvine.com'
+API_KEY = '4a54f2e9f860c0230764deb5c01ceea49fe98dde' # Default API key
+
+lf_category = 0
+lf_video = 0
+
+def get_categories():
+    categories = mc.ListItems()
+
+    # Live streams
+    response = mc.Http().Get(API_PATH + '/chats/?api_key=' + API_KEY + '&format=json')
+    chat_data = simplejson.loads(response)['results']
+
+    for chat in chat_data:
+        # src_url = urllib.quote('http://www.justin.tv/widgets/live_embed_player.swf?channel=' + chat['channel_name'] + '&auto_play=true&start_volume=25')
+        # if chat['password']:
+        #     src_url += '&publisherGuard=' + chat['password']
+        # bx_jsactions_url = urllib.quote('http://localhost:8001/media/chat/js/boxee.js')
+        # url = 'flash://www.justin.tv/src=' + src_url + '&bx-jsactions=' + bx_jsactions_url
+
+        url = 'http://www.justin.tv/widgets/live_embed_player.swf?channel=' + chat['channel_name'] + '&auto_play=true&start_volume=25'
+        if chat['password']:
+            url += '&publisherGuard=' + chat['password']
+
+        item = mc.ListItem(mc.ListItem.MEDIA_VIDEO_CLIP)
+        item.SetLabel('LIVE: ' + chat['title'].encode('utf-8'))
+        item.SetProperty('id', 'live'.encode('utf-8'))
+        item.SetDescription(chat['deck'].encode('utf-8'))
+        item.SetThumbnail(chat['image']['super_url'].encode('utf-8'))
+        item.SetPath(url.encode('utf-8'))
+        categories.append(item)
+
+    response = mc.Http().Get(API_PATH + '/video_types/?api_key=' + API_KEY + '&format=json')
+    category_data = simplejson.loads(response)['results']
+
+    # Latest
+    item = mc.ListItem(mc.ListItem.MEDIA_UNKNOWN)
+    item.SetLabel('Latest'.encode('utf-8'))
+    item.SetProperty('id', 'latest'.encode('utf-8'))
+    categories.append(item)
+
+    for cat in category_data:
+        item = mc.ListItem(mc.ListItem.MEDIA_UNKNOWN)
+        item.SetLabel(cat['name'].encode('utf-8'))
+        item.SetProperty('id', str(cat['id']).encode('utf-8'))
+        categories.append(item)
+
+    # Search
+    item = mc.ListItem(mc.ListItem.MEDIA_UNKNOWN)
+    item.SetLabel('Search'.encode('utf-8'))
+    item.SetProperty('id', 'search'.encode('utf-8'))
+    categories.append(item)
+
+    # Link Account
+    api_key = mc.GetApp().GetLocalConfig().GetValue('api_key')
+    if not api_key:
+        item = mc.ListItem(mc.ListItem.MEDIA_UNKNOWN)
+        item.SetLabel('Link Account'.encode('utf-8'))
+        item.SetProperty('id', 'link'.encode('utf-8'))
+        categories.append(item)
+
+    return categories
+
+def get_api_key(link_code):
+    if link_code and len(link_code) == 6:
+        try:
+            response = mc.Http().Get(API_PATH + '/validate?link_code=' + link_code + '&format=json')
+            data = simplejson.loads(response)
+            api_key = data['api_key']
+            return api_key
+        except:
+            return None
+    else:
+        return None
+
+def link_account():
+    mc.ShowDialogOk("Let's do this.", "To link your account, visit www.giantbomb.com/boxee to get a link code.\n\nEnter this code on the next screen.")
+
+    link_code = mc.ShowDialogKeyboard("Enter your link code.", "", False).upper()
+    new_api_key = get_api_key(link_code)
+    if new_api_key:
+        mc.GetApp().GetLocalConfig().SetValue('api_key', new_api_key)
+        global API_KEY
+        API_KEY = new_api_key
+        return True
+    else:
+        return False
+
+def get_videos(cat_id):
+    if cat_id == 'latest':
+        response = mc.Http().Get(API_PATH + '/videos/?api_key=' + API_KEY + '&sort=-publish_date&format=json')
+    elif cat_id == 'search':
+        query = mc.ShowDialogKeyboard("Search", "", False).replace(' ', '%20')
+        response = mc.Http().Get(API_PATH + '/search/?api_key=' + API_KEY + '&resources=video&query=' + query + '&format=json')
+    elif cat_id == '5-DP':
+        response = mc.Http().Get(API_PATH + '/videos/?api_key=' + API_KEY + '&video_type=5&offset=161&format=json')
+    elif cat_id == '5-P4':
+        response = mc.Http().Get(API_PATH + '/videos/?api_key=' + API_KEY + '&video_type=5&format=json')
+    elif cat_id == '5-MO':
+        response = mc.Http().Get(API_PATH + '/videos/?api_key=' + API_KEY + '&video_type=5&offset=105&limit=21&format=json')
+    else:
+        response = mc.Http().Get(API_PATH + '/videos/?api_key=' + API_KEY + '&video_type=' + cat_id + '&sort=-publish_date&format=json')
+
+    video_data = simplejson.loads(response)['results']
+
+    if cat_id == '5-P4':
+        response = mc.Http().Get(API_PATH + '/videos/?api_key=' + API_KEY + '&video_type=5&offset=100&limit=61&format=json')
+        video_data += simplejson.loads(response)['results']
+        video_data = [video for video in video_data if not video['name'].startswith('The Matrix Online')]
+    elif cat_id == '5-MO':
+        video_data = [video for video in video_data if video['name'].startswith('The Matrix Online')]
+
+    videos = mc.ListItems()
+
+    border = 'bg_imgFlare_640x360.png'.encode('utf-8')
+    for vid in video_data:
+        if 'hd_url' in vid:
+            url = vid['hd_url'] + '&api_key=' + API_KEY
+        else:
+            url = vid['high_url']
+        date = vid['publish_date'].encode('utf-8').split(' ')[0].split('-')
+        #duration = vid['length_seconds']
+
+        item = mc.ListItem(mc.ListItem.MEDIA_VIDEO_CLIP)
+        item.SetLabel(vid['name'].encode('utf-8'))
+        item.SetDescription(vid['deck'].encode('utf-8'))
+        item.SetThumbnail(vid['image']['super_url'].encode('utf-8'))
+        item.SetImage(0, border)
+        item.SetPath(url.encode('utf-8'))
+        item.SetDate(int(date[0]), int(date[1]), int(date[2]))
+        #item.SetDuration(int(duration).encode('utf-8'))
+        videos.append(item)
+
+        if border == 'bg_imgFlare_640x360.png'.encode('utf-8'):
+            border = 'bg_imgFlare2_640x360.png'.encode('utf-8')
+        else:
+            border = 'bg_imgFlare_640x360.png'.encode('utf-8')
+
+    return videos
---
+++ comicvine/start.py
@@ -0,0 +1,30 @@
+import mc
+import menu
+
+menu.lf_category = 0
+menu.lf_video = 0
+
+mc.ActivateWindow(14000)
+
+api_key = mc.GetApp().GetLocalConfig().GetValue('api_key')
+if api_key:
+    menu.API_KEY = api_key
+
+categories = menu.get_categories()
+if categories:
+    mc.GetWindow(14000).GetList(100).SetItems(categories)
+
+mc.GetActiveWindow().GetList(100).SetFocusedItem(0)
+
+cat_id = mc.GetFocusedItem(14000, 100).GetProperty('id')
+
+if cat_id == 'live':
+    pass
+else:
+    mc.GetActiveWindow().PushState()
+    mc.ShowDialogWait()
+    items = menu.get_videos(cat_id)
+    mc.GetActiveWindow().GetList(120).SetItems(items)
+    mc.GetActiveWindow().GetControl(120).SetVisible(True)
+    mc.HideDialogWait()
+    mc.GetActiveWindow().GetControl(120).SetFocus()
