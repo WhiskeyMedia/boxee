00efd3abf7231f22dc3b7373c75389249be1ce1e
sdornan <sam@whiskeymedia.com>
2011-08-23 17:35:00 -0700

    Change the API key back and don't display link account button if an account is already linked

--- giantbomb/menu.py
+++ giantbomb/menu.py
@@ -3,7 +3,7 @@ import simplejson
 import urllib
 
 API_PATH = 'http://api.giantbomb.com'
-API_KEY = 'e5529a761ee3394ffbd237269966e9f53a4c7bf3' # Default API key
+API_KEY = '57659bff0faacdf8ab9b2500b552e7fd37b4b677' # Default API key
 
 lf_category = 0
 lf_video = 0
@@ -56,10 +56,12 @@ def get_categories():
     categories.append(item)
 
     # Link Account
-    item = mc.ListItem(mc.ListItem.MEDIA_UNKNOWN)
-    item.SetLabel('Link Account'.encode('utf-8'))
-    item.SetProperty('id', 'link'.encode('utf-8'))
-    categories.append(item)
+    api_key = mc.GetApp().GetLocalConfig().GetValue('api_key')
+    if not api_key:
+        item = mc.ListItem(mc.ListItem.MEDIA_UNKNOWN)
+        item.SetLabel('Link Account'.encode('utf-8'))
+        item.SetProperty('id', 'link'.encode('utf-8'))
+        categories.append(item)
 
     return categories
 
